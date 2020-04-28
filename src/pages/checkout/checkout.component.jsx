73b582de92f25fd871880dc83c433c3a78700861
Raf Andrzejczyk <raf@digitalperformance.io>
2020-04-28 08:53:06 +0100

    stripe payment integration, favicon and title change

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,7 @@ import {
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -30,9 +31,14 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      <br />
+      *Please use the following test credit cart for payments*
+      <br />
+      4242 4242 4242 4242 - exp: 12/20 - CVV: 123
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
