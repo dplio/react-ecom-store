557509a204b64acbff400f51f0cef733aa0906d8
Raf Andrzejczyk <raf@digitalperformance.io>
2020-05-03 15:50:38 +0100

    package update

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 import "./directory.styles.scss";
-import MenuItem from "../../components/menu-item/menu-item.component";
+import MenuItem from "../menu-item/menu-item.component";
 
 const Directory = ({ sections }) => (
   <div className="directory-menu">
